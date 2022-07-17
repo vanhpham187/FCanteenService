60c04578a55f7979ca13b4b4b32341e83ae54d65
chaubnmhe153019 <chaubnmhe153019@fpt.edu.vn>
2022-07-17 23:01:59 +0700

    admin

--- src/main/java/fcantenn/repository/IUserRepository.java
+++ src/main/java/fcantenn/repository/IUserRepository.java
@@ -4,7 +4,12 @@ import fcantenn.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IUserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String email);
+
+    List<User> findAll();
+    void deleteById(Integer integer);
 }
